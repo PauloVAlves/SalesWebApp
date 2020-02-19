734659d607ca2fc3c2761b1dfb72c1a653f1a65c
Paulo Alves <paulovalves@outlook.com>
2020-02-19 16:00:23 -0300

    Project Created

---
+++ SalesWebMVCUI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SalesWebMVCUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
